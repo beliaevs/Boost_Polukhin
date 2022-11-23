242f876408adce3a06e31a0ce3d3cf33a813f7be
Alexander Beliaev <beliaevs@gmail.com>
2022-11-23 18:18:40 +0300

    small vector and static vector demo

---
+++ containers_piotr_rut/main.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<boost/container/small_vector.hpp>
+#include<boost/container/static_vector.hpp>
+
+class Spy
+{
+    public:
+    explicit Spy(int n): d_n(n) 
+    {
+        std::cout << "Spy(" << d_n << ")\n";
+    }
+
+    Spy(const Spy& other): d_n(other.d_n)
+    {
+        std::cout << "Spy copy(" << d_n << ")\n";
+    }
+
+    Spy& operator = (const Spy& i_other)
+    {
+        std::cout << "Spy(" << d_n << ") = Spy(" << i_other.d_n << ")\n";
+        d_n = i_other.d_n;
+        return *this;
+    }
+
+    ~Spy()
+    {
+        std::cout << "~Spy(" << d_n << ")\n";
+    }
+
+    int get() const
+    {
+        return d_n;
+    }
+
+    private:
+    int d_n;
+};
+
+int main()
+{
+    {
+        boost::container::small_vector<Spy, 4> v;
+        std::cout << v.capacity() << "\n";
+        v.emplace_back(1);
+        std::cout << v.size() << ", " << v.capacity() << "\n";
+        for(int i = 2; i < 8; ++i)
+        {
+            v.emplace_back(i);
+        }
+    }
+    {
+        boost::container::static_vector<Spy, 4> v;
+        for(int i = 0; i < 4; ++i)
+        {
+            v.emplace_back(i);
+        }
+        try
+        {
+          v.emplace_back(5);
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << e.what() << '\n';
+        }
+    }
+}
